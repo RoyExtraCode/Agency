e3471beb0efce0c5a36eab0b9e40aab6eb5e4b4d
Roy Allen <royextracode@gmail.com>
2014-05-07 04:05:55 -0700

    Added basic files to project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('agencyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
